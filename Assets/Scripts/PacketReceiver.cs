6df85eb9450d13563307467658ff7a389cad8206
Konrad Klocek <xqwzts251@gmail.com>
2017-04-22 14:19:08 +0200

    Add UDP receiver;skeleton rendering

--- Assets/Scripts/PacketReceiver.cs
+++ Assets/Scripts/PacketReceiver.cs
@@ -1,16 +1,63 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
 using UnityEngine;
+using UnityEngine.Networking;
 
 public class PacketReceiver : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    [SerializeField]
+    private int port = 8080;
+    [SerializeField]
+    private string host = "192.168.0.2";
+
+    UdpClient client;
+    string text;
+    IPAddress server;
+    IPEndPoint remoteEP;
+    private bool flag = false;
+
+    public String Data
+    {
+        get { return text; }
+    }
+
+    void Start()
+    {
+        Debug.Log("Starting Client");
+        server = IPAddress.Parse(host);
+        remoteEP = new IPEndPoint(IPAddress.Any, 0);
+        client = new UdpClient(port);
+        //client.JoinMulticastGroup(groupIP);
+        client.Client.Blocking = false;
+        client.BeginReceive(new AsyncCallback(ReceiveServerInfo), null);
+
+    }
+
+    private void Update()
+    {
+        if(flag)
+        {
+            client.BeginReceive(new AsyncCallback(ReceiveServerInfo), null);
+            flag = false;
+        }
+    }
+
+    void ReceiveServerInfo(IAsyncResult result)
+    {
+        //Debug.Log("Received Server Info");
+        byte[] receivedBytes = client.EndReceive(result, ref remoteEP);
+        //byte[] data = client.Receive(ref remoteEP);
+
+        // encode UTF8-coded bytes to text format
+        text = Encoding.ASCII.GetString(receivedBytes);
+
+        // show received message
+        //if (text != null && text != "")
+        //    Debug.Log(">> " + text);
+        flag = true;
+    }
 }
---
+++ Assets/Scripts/SkeletonManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkeletonManager : MonoBehaviour {
+
+    [SerializeField]
+    private Transform[] spheres;
+    [SerializeField]
+    private PacketReceiver packetReceiver;
+	// Use this for initialization
+	void Start () {
+        Transform [] allTransforms = GetComponentsInChildren<Transform>();
+        spheres = new Transform[allTransforms.Length - 1];
+        for(int i = 0, j = 0; i < allTransforms.Length; i++, j++)
+        {
+            if(allTransforms[i] == transform)
+            {
+                spheres[j] = allTransforms[i + 1];
+                i++;
+            } else
+            {
+                spheres[j] = allTransforms[i];
+            }
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (packetReceiver.Data == null)
+            return;
+
+        string [] data = packetReceiver.Data.Split(';');
+        float[] floats = new float[data.Length - 1];
+        for(int i = 0; i < data.Length - 1; i++)
+        {
+            floats[i] = float.Parse(data[i]);
+        }
+
+        for(int i = 0, j = 0; i < floats.Length; i += 3, j++)
+        {
+            spheres[j].position = new Vector3(floats[i], floats[i + 1], floats[i + 2]) * 5;
+        }
+	}
+}
